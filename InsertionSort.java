e22922ffa4a2c99611caa02d4e92705b05616b3e
Francis Sirizzotti <francis@sirizzotti.info>
2021-11-22 10:47:27 -0500

    Oops, bug.

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,7 +2,7 @@ public class InsertionSort implements SortingAlgo {
     public void sort(int[] sortMe) {
         for (int i = 1; i < sortMe.length; i++) {
             int j = i;
-            while (sortMe[j] < sortMe[j-1]) {
+            while (j > 0 && sortMe[j] < sortMe[j-1]) {
                 int tmp = sortMe[j];
                 sortMe[j] = sortMe[j-1];
                 sortMe[--j] = tmp;
