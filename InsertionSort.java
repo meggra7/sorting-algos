df0ef2f34eb3d2decef5c025b30171c796fc8bea
Meghan Dunham <meghan.g.dunham@gmail.com>
2021-11-29 21:57:05 -0500

    Update InsertionSort.java
    This is similar to a bubble sort, but technically sifts down values rather than bubbling them up.  Compared to a traditional bubble sort, it also iterates from the lowest index up but then sifts down as it reaches new values. While the overall iteration through the indices is pretty standard, the subsequent comparisons moving from the current index back down to 0 were not as clear since decrementing 'j' was done during the swap operation.  So my best recommendation to make this more clear, while maintaining the original logic, would be to move out the swap operation into its own method.  Then, decrement 'j' on its own line so that the downward movement of the comparison index is immediately apparent.

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,12 +1,18 @@
 public class InsertionSort implements SortingAlgo {
+    
     public void sort(int[] sortMe) {
         for (int i = 1; i < sortMe.length; i++) {
             int j = i;
             while (j > 0 && sortMe[j] < sortMe[j-1]) {
-                int tmp = sortMe[j];
-                sortMe[j] = sortMe[j-1];
-                sortMe[--j] = tmp;
+                swap(sortMe, j);
+                j--;
             }
         }
     }
+    
+    private void swap(int[] list, int i) {
+        int tmp = list[i];
+        list[i] = list[i - 1];
+        list[i - 1] = tmp;
+    }        
 }
